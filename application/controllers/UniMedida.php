3f286cf4fb011085d0b46dbc23dc83a6640f6fa4
Allysson1 <81646221+Allysson1@users.noreply.github.com>
2022-12-07 20:31:51 -0300

    adiciona unidade de medida

--- application/controllers/UniMedida.php
+++ application/controllers/UniMedida.php
@@ -1,10 +1,12 @@
 <?php
 
-defined('BASEPATH') or exit ('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class UniMedida extends CI_Controller {
+class UniMedida extends CI_Controller
+{
 
-    public function inserir() {
+    public function inserir()
+    {
 
         //sigla e descrição recebidas via JSON e colocadas em variáveis
         //Retornos possiveis:
@@ -25,25 +27,167 @@ class UniMedida extends CI_Controller {
 
         //faremos uma validação para sabermos se todos os dados foram enviados corretamente
 
-        if(trim($sigla) == ''){
-            $retorno = array('codigo' => 2,
-                                'msg' => 'Sigla não informada.');
-            )
+        if (trim($sigla) == '') {
+            $retorno = array(
+                'codigo' => 2,
+                'msg' => 'Sigla não informada.'
+            );
+        } elseif (strlen(trim($sigla)) > 3) {
+            $retorno = array(
+                'codigo' => 3,
+                'msg' => 'Sigla pode conter no máximo 3 caracteres.'
+            );
+        } elseif (trim($descricao) == '') {
+            $retorno = array(
+                'codigo' => 4,
+                'msg' => 'Descrição não informada'
+            );
+        } elseif (trim($usuario == '')) {
+            $retorno = array(
+                'codigo' => 5,
+                'msg' => 'Usuário não informado'
+            );
+        } else {
+            $this->load->model('m_unidmedida');
+            $retorno = $this->m_unidmedida->inserir($sigla, $descricao, $usuario);
         }
-        elseif (strlen(trim($sigla)) > 3) {
-            $retorno = array('codigo' => 3,
-                                'msg' => 'Sigla pode conter no máximo 3 caracteres.');
-        }
-        elseif (trim($descricao) == '') {
-            $retorno = array('codigo' => 4,
-                                'msg' => 'Descrição não informada');
-        }
-        elseif(trim($usuario == '')){
 
+        echo json_encode($retorno);
+    }
+
+
+
+    public function consultar()
+    {
+        //código, sigla e descrição
+        //codigo via SJON e colocados em variaveis
+        //Retornos possiveis:
+        //1 - Dados consultados corretamente (banco)
+        //2 - Quantidade de caracteres da sigla é superior a 3 (frontend)
+        //6 - Dados não encontrados (banco)
+
+        $json = file_get_contents('php://input');
+        $resultado = json_decode($json);
+
+        $codigo = $resultado->codigo;
+        $sigla = $resultado->sigla;
+        $descricao = $resultado->descricao;
+
+        //verifico somente a quantidade de caracteres da sigla, pode ter até 3 
+        //caracters ou nenhum para trazer todas as siglas
+        if (strlen(trim($sigla)) > 3) {
+            $retorno = array(
+                'codigo' => 2,
+                'msg' => 'Sigla pode conter no máximo 3 caracteres ou nenhum para todas'
+            );
+        } else {
+            //realizo a instância da Model
+            $this->load->model('m_unidmedida');
+
+            $retorno = $this->m_unidmedida->consultar($codigo, $sigla, $descricao);
         }
+
+        echo json_encode($retorno);
+    }
+
+
+
+
+
+    //criando o metodo alterar
+    public function alterar(){
+        //usuario nome, senha e tipo (administrador comum) recebidos via JSON e colocado em variaveis
+        //retornos possiveis:
+        //1 - dado(s) alterados(s) com sucesso (banco)
+        //2 - 
+        //3 - 
+        //4 - 
+        //5 - usuário não informado(frontend)
+        //6 - Dados não encontrados (banco)
+        //7 - Houve problema no salvamento do LOG, mas o usuário foi alterado (LOG)
+        
+
+        $json = file_get_contents('php://input');
+        $resultado = json_decode($json);
+
+        $codigo = $resultado->codigo;
+        $sigla = $resultado->sigla;
+        $descricao = $resultado->descricao;
+        $usuario = $resultado->tipo_usuario;
+
+    
+
+         //Faremos uma validação para sabermos se todos os dados foram enviados.
+ 
+        if (trim($codigo) == '') {
+             $retorno = array ('codigo' => 2,
+                                 'msg' => 'codigo não informado');
+         }
+        
+        
+        elseif (strlen(trim($sigla)) > 3){
+                $retorno = array('codigo' => 3,
+                'msg' => 'Sigla pode conter no máximo 3 caracteres');
+        } 
+            
+        elseif (trim($descricao) == '' && trim($sigla) == ''){
+           $retorno = array('codigo' => 4,
+            'msg' => 'sigla e descrição não foram informadas');
+        } 
+        
+        elseif(trim($usuario) == ''){
+            $retorno = array('codigo' => 5,
+            'msg' => 'Usuario não infomado');
+         } 
+
+        else{
+                   //realizo a instância da Model
+            $this->load->model('m_unidmedida');
+
+            //atribuindo ao $retorno o array com informações da validação do acesso
+            $retorno = $this->m_unidmedida->alterar($codigo, $sigla, $descricao, $usuario);
+        }
+        //Retorno no formato JSON
+        echo json_encode($retorno);
 
     }
 
+
+
+    //função desativar
+    public function desativar(){
+     
+        //Retornos possiveis:
+        // 1 - 
+        // 2 - 
+        // 6 - dados não encontrados (banco)
+
+        $json = file_get_contents('php://input');
+        $resultado = json_decode($json);
+
+        $usuario = $resultado->usuario;
+        $codigo = $resultado->codigo;
+
+         
+         if (trim($codigo) == '') {
+            $retorno = array ('codigo' => 2,
+                                'msg' => 'codigo da unidade não informado');
+        }
+    
+        elseif (trim($usuario) == ''){
+            $retorno = array('codigo' => 5,
+                            'msg' => 'Usuairio não informado');
+        }
+        else{
+        //realizo a instância da Model
+        $this->load->model('m_unidmedida');
+
+        //atribuindo ao $retorno o array com informações da validação do acesso
+        $retorno = $this->m_unidmedida->desativar($usuario, $codigo);
+    }
+    //Retorno no formato JSON
+    echo json_encode($retorno);
+    }
 
 
 }
--- application/models/m_acesso.php
+++ application/models/m_acesso.php
@@ -3,26 +3,64 @@
 defined('BASEPATH') or exit ('No direct script acess allowed');
 
 class m_acesso extends CI_Model {
+
     public function validalogin($usuario, $senha){
 
-        $retorno = $this->db->query("select * from usuarios
-                                        where usuario = '$usuario'
-                                        and senha = md5('$senha')
-                                        and estatus = ''");
+        $retorno = $this->db->query("select * from usuarios where usuario = '$usuario'");
 
-        if ($retorno-> num_rows() > 0){
-            $dados = array('codigo' => 1,
-                            'msg' => 'Usuário correto');
+        if ($retorno-> num_rows() > 0) {
+
+            if ($retorno->row()->estatus == 'D'){
+                $dados = array ('Codigo' => 6,
+                                'msg' => 'usuario desativado, não pode ser logado.');
+            }
+    
+            elseif ($retorno->row()->senha != $senha){//não consegui fazer a validação da senha encriptada com md5
+                $dados = array('codigo' => 4,
+                                'msg' => 'Senha Incorreta');
+            }
+            
+            else{
+                $dados = array('codigo' => 8,
+                                'msg' => 'Usuário e senha estão corretos.');
+            }
+            
         }
         else{
-            $dados = array('codigo' => 4,
-                            'msg' => 'Usuário ou senha inválidos.');
+            $dados = array('codigo' => 5,
+                            'msg' => 'usuario incorreto');
         }
 
+
+
+
+
+
+        
+        // if ($retorno-> num_rows() > 0 && $retorno = $usuario && $retorno = $senha){
+            
+        //     $sql = $this->db->query("select estatus from usuarios
+        //     where usuario ='$usuario'
+        //       and estatus != 'D'");
+
+        //     if($sql != ''){
+        //         $dados = array('codigo' => 6,
+        //                     'msg' => 'Usuario Correto');
+        //     }
+
+        //     else{
+        //         $dados = array('codigo' => 7,
+        //         'msg' => 'Usuario desabilitado para acesso');
+        //     }
+        // }
+
+
+
+        
         return $dados;
 
-
     }
+    
 }
 
 ?>
